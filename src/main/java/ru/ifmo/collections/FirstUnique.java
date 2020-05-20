2b562e57cdc1d80d0755e1395e284b3d3246bfb4
Nikita <Nik2000kop@gmail.com>
2020-05-20 18:27:42 +0300

    add implements to all classes

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,40 @@
 package ru.ifmo.collections;
 
-/**
- * Design a class which contains integers and returns first unique integer (in order of addition).
- * FirstUniqueTest can be used as an example.
- */
+import java.util.*;
+
 public class FirstUnique {
-    public FirstUnique(int[] numbers) {
-        // TODO implement
+    private final Map<Integer, Integer> numbersCountMap;
+    private final Queue<Integer> numbers;
+
+    public FirstUnique(int[] numbers) throws IllegalArgumentException {
+        if (numbers == null) {
+            throw new IllegalArgumentException("initial array can't be null");
+        }
+        numbersCountMap = new HashMap<>();
+        this.numbers = new ArrayDeque<>();
+        for (int number : numbers) {
+            add(Integer.valueOf(number));
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        while (!numbers.isEmpty() && numbersCountMap.get(numbers.element()) > 1) {
+            numbers.remove();
+        }
+        if (numbers.isEmpty()) {
+            return -1;
+        }
+        return numbers.element();
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        numbers.add(value);
+        if (numbersCountMap.containsKey(value)) {
+            int currentCount = numbersCountMap.get(value);
+            numbersCountMap.replace(value, currentCount + 1);
+        }
+        else {
+            numbersCountMap.put(value, 1);
+        }
     }
 }
--- src/main/java/ru/ifmo/collections/KthLargest.java
+++ src/main/java/ru/ifmo/collections/KthLargest.java
@@ -1,18 +1,28 @@
 package ru.ifmo.collections;
 
-/**
- * Design a class to find the kth largest element in a stream. k is from 1 to numbers.length.
- * Note that it is the kth largest element in the sorted order, not the kth distinct element.
- *
- * Constructor accepts an integer k and an integer array numbers, which contains initial elements from the stream.
- * For each call to the method KthLargest.add(), return the element representing the kth largest element in the stream.
- */
+import java.util.*;
+
 public class KthLargest {
+    private final Queue<Integer> minHeap;
+
     public KthLargest(int k, int[] numbers) {
-        // TODO implement
+        if (numbers == null || numbers.length == 0) {
+            throw new IllegalArgumentException("initial array can't be null/empty");
+        }
+        minHeap = new PriorityQueue<>();
+        for (int number : numbers) {
+            minHeap.add(number);
+        }
+        while (minHeap.size() != k) {
+            minHeap.remove();
+        }
     }
 
     public int add(int val) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (val > minHeap.element()) {
+            minHeap.remove();
+            minHeap.add(val);
+        }
+        return minHeap.element();
     }
 }
